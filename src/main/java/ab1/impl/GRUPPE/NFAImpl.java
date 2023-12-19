0a4be221f69eed5aae839f99c04d2e671cc39ecc
Markus Teichmann <markuste@edu.aau.at>
2023-12-19 09:55:28 +0100

    NFAImpl bearbeitet. acceptsWord, addAcceptingState, addTransition, getAcceptingStates, getStates - TODO: Fehlerbehandlung ^^

---
+++ src/main/java/ab1/impl/GRUPPE/NFAImpl.java
@@ -0,0 +1,188 @@
+package ab1.impl.GRUPPE;
+
+import ab1.FinalizedStateException;
+import ab1.NFA;
+import ab1.Transition;
+
+import java.util.*;
+
+public class NFAImpl implements NFA {
+    private State initialState;
+    private Map<String,State> states;
+    private List<Transition> transitions;
+    public NFAImpl(String initialStateName) {
+        this.initialState = new State(initialStateName);
+        this.states = new HashMap<String,State>();
+        this.transitions = new ArrayList<Transition>();
+    }
+    @Override
+    public Set<String> getStates() {
+        return this.states.keySet();
+    }
+
+    @Override
+    public Set<String> getAcceptingStates() {
+        Set<String> states = new HashSet<String>();
+        for(State state : this.states.values()) {
+            if(state.getAcceptence() == State.Acceptance.ACCEPTING) {
+                states.add(state.getName());
+            }
+        }
+        return states;
+    }
+
+    @Override
+    public String getInitialState() {
+        return initialState.getName();
+    }
+
+    @Override
+    public void addTransition(Transition transition) throws FinalizedStateException {
+        /*
+        Hier fehlt noch die Fehlerbehandlung.
+         */
+        if(!(this.transitions.contains(transition))) {
+            this.transitions.add(transition);
+            if(!(this.states.containsKey(transition.fromState()))) {
+                this.states.put(transition.fromState(), new State(transition.fromState()));
+            }
+            if(!(this.states.containsKey(transition.toState()))) {
+                this.states.put(transition.toState(), new State(transition.toState()));
+            }
+        }
+    }
+
+    @Override
+    public void addAcceptingState(String state) throws FinalizedStateException {
+        /*
+        Hier fehlt auch noch die Fehlerbehandlung.
+         */
+        if(this.states.containsKey(state)) {
+            this.states.get(state).setAcceptance(State.Acceptance.ACCEPTING);
+        } else {
+            this.states.put(state,new State(state, State.Acceptance.ACCEPTING));
+        }
+    }
+
+    @Override
+    public NFA union(NFA other) throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public NFA intersection(NFA other) throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public NFA concatenation(NFA other) throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public NFA kleeneStar() throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public NFA plusOperator() throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public NFA complement() throws FinalizedStateException {
+        return null;
+    }
+
+    @Override
+    public boolean isFinalized() {
+        return false;
+    }
+
+    @Override
+    public void finalizeAutomaton() {
+
+    }
+
+    @Override
+    public boolean isFinite() {
+        return false;
+    }
+
+    @Override
+    public boolean acceptsWord(String word) {
+        List<String> currentlyReachedStates = new ArrayList<String>();
+        currentlyReachedStates.add(initialState.getName());
+        while(word.length() > 0) {
+            for(String state : currentlyReachedStates) {
+                currentlyReachedStates.remove(state);
+                currentlyReachedStates.addAll(getReachableStates(state, word.charAt(0)));
+            }
+            word = word.substring(1);
+        }
+        for(String state : currentlyReachedStates) {
+            if(this.states.get(state).getAcceptence() == State.Acceptance.ACCEPTING) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*
+    Diese Methode gibt alle von einem Startzustand über eine spezifische Kante erreichbare Knoten zurück.
+    Hierbei werden auch die Epsilon-Kanten berücksichtigt. Somit ist der Abstand auch nicht mehr nur 1,
+    denn es können ja auch mehrere Epsilon-Kanten aufeinander folgen.
+     */
+    private Set<String> getReachableStates(String state, Character c) {
+        Set<String> reachableStates = getWithOneCharacterTransitionReachableStates(state, c);
+        Set<String> byEpsilonReachableStates = new HashSet<String>();
+        int currentsize;
+        do { // Do Schleife ist hier wichtig, da wir mindestens einmal durchlaufen müssen
+            currentsize = reachableStates.size();
+            byEpsilonReachableStates.clear();
+            for(String reachedState : reachableStates) {
+                byEpsilonReachableStates.addAll(getWithOneCharacterTransitionReachableStates(reachedState, 'e'));
+            }
+            reachableStates.addAll(byEpsilonReachableStates);
+        } while(currentsize < reachableStates.size());
+        return reachableStates;
+    }
+    /*
+    Diese Methode gibt alle von einem Startknoten aus über eine spezifische Kante erreichbaren Knoten zurück.
+     */
+    private Set<String> getWithOneCharacterTransitionReachableStates(String state, Character c) {
+        Set<Transition> stateRelatedTransitions = transitionsFrom(state);
+        Set<Transition> characterRelatedTransitions = transitionsWith(c);
+        Set<Transition> relevantTransitions = new HashSet<Transition>(stateRelatedTransitions);
+        relevantTransitions.retainAll(characterRelatedTransitions); // Mengenoperation Geschnitten.
+        Set<String> reachedStates = new HashSet<String>();
+        for(Transition t : relevantTransitions) {
+            reachedStates.add(t.toState());
+        }
+        return reachedStates;
+    }
+    /*
+    Diese Methode gibt alle Übergänge zu einem bestimmten Startknoten zurück.
+     */
+    private Set<Transition> transitionsFrom(String state) {
+        Set<Transition> transitions = new HashSet<Transition>();
+        for(Transition transition : this.transitions) {
+            if(transition.fromState().equals(state)) {
+                transitions.add(transition);
+            }
+        }
+        return transitions;
+    }
+    /*
+    Diese Methode gibt alle Übergänge mit einem bestimmten Übergang zurück.
+     */
+    private Set<Transition> transitionsWith(Character c) {
+        Set<Transition> transitions = new HashSet<Transition>();
+        for (Transition transition : this.transitions) {
+            if (transition.readSymbol() == c) {
+                transitions.add(transition);
+            }
+        }
+        return transitions;
+    }
+}
---
+++ src/main/java/ab1/impl/GRUPPE/State.java
@@ -0,0 +1,30 @@
+package ab1.impl.GRUPPE;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class State {
+    private String name;
+    public enum Acceptance {
+        ACCEPTING,
+        DENYING
+    }
+    private Acceptance acceptance;
+    public State(String name) {
+        this.name = name;
+        this.acceptance = Acceptance.DENYING;
+    }
+    public State(String name, Acceptance acceptance) {
+        this.name = name;
+        this.acceptance = Acceptance.ACCEPTING;
+    }
+    public void setAcceptance(Acceptance acceptance){
+        this.acceptance = acceptance;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public Acceptance getAcceptence() {
+        return this.acceptance;
+    }
+}
