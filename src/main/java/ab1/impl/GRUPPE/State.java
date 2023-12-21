712b516897a2afbf537000859b7901e2849bf719
David Hude <davidhu@edu.aau.at>
2023-12-21 09:55:56 +0100

    solved conflicts

--- src/main/java/ab1/impl/GRUPPE/State.java
+++ src/main/java/ab1/impl/GRUPPE/State.java
@@ -1,34 +1,37 @@
 package ab1.impl.GRUPPE;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.Map;
 
 public class State {
     private String name;
+
     public enum Acceptance {
         ACCEPTING,
         DENYING
     }
+
     private Acceptance acceptance;
+
     public State(String name) {
         this.name = name;
         this.acceptance = Acceptance.DENYING;
     }
+
     public State(String name, Acceptance acceptance) {
         this.name = name;
         this.acceptance = Acceptance.ACCEPTING;
     }
-    public void setAcceptance(Acceptance acceptance){
+
+    public void setAcceptance(Acceptance acceptance) {
         this.acceptance = acceptance;
     }
+
     public String getName() {
         return this.name;
     }
+
     public Acceptance getAcceptence() {
         return this.acceptance;
     }
-=======
-public class State {
->>>>>>> 3f508f93a4ba0df1082ee0f83607a7a317144ba6
 }
